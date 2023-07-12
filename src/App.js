57e551d95ad554d0547d30c1835e2bc1d79cf099
Hazel <secretwish5@naver.com>
2023-07-12 17:16:49 +0900

    copy

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import SayHello from './SayHello';
+import Counter from './component/chap03_state/Counter';
+import NickName from './component/chap03_state/NickName';
 
 function App() {
+
+  const looping = () => {
+    const helloList = [];
+    for (let i = 0; i < 5; i++) {
+      helloList.push(<SayHello />);
+    }
+    return helloList;
+  };
+
+  // jsx문법에서는 스크립트를 코드를 직접 사용 불가능
+  // {} 안에서 함수호출문이나 변수참조를 할 수 있음.
   return (
-  <h1>
-    리엑트 초보입ㄴ다
-  </h1>
+      <>
+        <Counter />
+      </>
   );
 }
 
